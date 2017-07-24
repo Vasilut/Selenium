5789bcc32451dc1aa4eeebfd5375a2cc6611707e
Vasilut Lucian <lucian.vasilut10@gmail.com>
2017-07-24 21:10:51 +0300

    Added NUnit and a mock test.

--- SeleniumProj/SeleniumProj/Program.cs
+++ SeleniumProj/SeleniumProj/Program.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
@@ -8,16 +9,44 @@ using System.Threading.Tasks;
 
 namespace SeleniumProj
 {
-    class Program
+    public class Program
     {
+        //create the reference for our browser
+        IWebDriver driver = new ChromeDriver();
+
         static void Main(string[] args)
         {
-            //create the reference for our browser
-            IWebDriver driver = new ChromeDriver();
+            ////create the reference for our browser
+            //IWebDriver driver = new ChromeDriver();
 
+            ////navigate to a page
+            //driver.Navigate().GoToUrl("https://www.prophetondemand.com/prophet/admin/");
+
+            ////Find the element
+            //IWebElement name = driver.FindElement(By.Name("_ctl0:ContentPlaceHolder1:tb_Email"));
+            //IWebElement password = driver.FindElement(By.Name("_ctl0:ContentPlaceHolder1:tb_Password"));
+            //IWebElement loginButton = driver.FindElement(By.Name("_ctl0:ContentPlaceHolder1:btn_signin"));
+
+
+            ////Perform ops
+            //name.SendKeys("lucian.vasilut10@gmail.com");
+            //password.SendKeys("11111");
+            //loginButton.SendKeys(Keys.Enter);
+
+
+            //driver.Quit();
+        }
+
+        [SetUp]
+        public void Initialize()
+        {
             //navigate to a page
-            driver.Navigate().GoToUrl("http://192.168.111.196/prophet/admin/login.aspx");
+            driver.Navigate().GoToUrl("https://www.prophetondemand.com/prophet/admin/");
+        }
 
+        [Test]
+        public void ExecuteTest()
+        {
             //Find the element
             IWebElement name = driver.FindElement(By.Name("_ctl0:ContentPlaceHolder1:tb_Email"));
             IWebElement password = driver.FindElement(By.Name("_ctl0:ContentPlaceHolder1:tb_Password"));
@@ -28,9 +57,14 @@ namespace SeleniumProj
             name.SendKeys("lucian.vasilut10@gmail.com");
             password.SendKeys("11111");
             loginButton.SendKeys(Keys.Enter);
-
-
-            driver.Quit();
         }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Quit();
+            //driver.Close();
+        }
+
     }
 }
