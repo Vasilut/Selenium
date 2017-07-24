f00a85a48e4880c1bf1a1f28afd5e488663a39b8
Vasilut <lucian.vasilut10@gmail.com>
2017-07-24 17:28:58 +0300

    Added packages and mock test.

--- SeleniumProj/SeleniumProj/Program.cs
+++ SeleniumProj/SeleniumProj/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +12,25 @@ namespace SeleniumProj
     {
         static void Main(string[] args)
         {
+            //create the reference for our browser
+            IWebDriver driver = new ChromeDriver();
 
+            //navigate to a page
+            driver.Navigate().GoToUrl("http://192.168.111.196/prophet/admin/login.aspx");
+
+            //Find the element
+            IWebElement name = driver.FindElement(By.Name("_ctl0:ContentPlaceHolder1:tb_Email"));
+            IWebElement password = driver.FindElement(By.Name("_ctl0:ContentPlaceHolder1:tb_Password"));
+            IWebElement loginButton = driver.FindElement(By.Name("_ctl0:ContentPlaceHolder1:btn_signin"));
+
+
+            //Perform ops
+            name.SendKeys("lucian.vasilut10@gmail.com");
+            password.SendKeys("11111");
+            loginButton.SendKeys(Keys.Enter);
+
+
+            driver.Quit();
         }
     }
 }
