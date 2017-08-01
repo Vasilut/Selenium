5c89406f95b2dd82b38dff8b9af531b5d3fc9ffb
Vasilut <lucian.vasilut10@gmail.com>
2017-08-01 10:34:23 +0300

    Added MicrosoftUnitTest like reference.

--- SeleniumProj/SeleniumProj/Program.cs
+++ SeleniumProj/SeleniumProj/Program.cs
@@ -17,22 +17,25 @@ namespace SeleniumProj
         static void Main(string[] args)
         {
             ////create the reference for our browser
-            //IWebDriver driver = new ChromeDriver();
+            IWebDriver driver = new ChromeDriver();
 
-            ////navigate to a page
-            //driver.Navigate().GoToUrl("https://www.prophetondemand.com/prophet/admin/");
+            //navigate to a page
+            driver.Navigate().GoToUrl("http://192.168.111.196/prophet/admin/saas/main.aspx");
 
-            ////Find the element
-            //IWebElement name = driver.FindElement(By.Name("_ctl0:ContentPlaceHolder1:tb_Email"));
-            //IWebElement password = driver.FindElement(By.Name("_ctl0:ContentPlaceHolder1:tb_Password"));
-            //IWebElement loginButton = driver.FindElement(By.Name("_ctl0:ContentPlaceHolder1:btn_signin"));
+            //Find the element
+            IWebElement name = driver.FindElement(By.Name("_ctl0:ContentPlaceHolder1:tb_Email"));
+            IWebElement password = driver.FindElement(By.Name("_ctl0:ContentPlaceHolder1:tb_Password"));
+            IWebElement loginButton = driver.FindElement(By.Name("_ctl0:ContentPlaceHolder1:btn_signin"));
 
 
-            ////Perform ops
-            //name.SendKeys("lucian.vasilut10@gmail.com");
-            //password.SendKeys("11111");
-            //loginButton.SendKeys(Keys.Enter);
+            //Perform ops
+            name.SendKeys("lucian.vasilut10@gmail.com");
+            password.SendKeys("11111");
+            loginButton.SendKeys(Keys.Enter);
 
+            //Find the login user
+            IWebElement userLoggedIn = driver.FindElement(By.Id("Top1_lblFirstNM"));
+            var userName = userLoggedIn.Text;
 
             //driver.Quit();
         }
@@ -46,7 +49,7 @@ namespace SeleniumProj
 
         [Test]
         public void ExecuteTest()
-        {
+        { 
             //Find the element
             IWebElement name = driver.FindElement(By.Name("_ctl0:ContentPlaceHolder1:tb_Email"));
             IWebElement password = driver.FindElement(By.Name("_ctl0:ContentPlaceHolder1:tb_Password"));
